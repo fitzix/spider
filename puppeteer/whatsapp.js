ab1d66102296b9dedc123c6d507a0ade049d4376
fitzix <caojunkaiv@gmail.com>
2020-05-30 00:32:10 +0800

    feat: x

--- puppeteer/whatsapp.js
+++ puppeteer/whatsapp.js
@@ -5,7 +5,7 @@ const puppeteer = require('puppeteer');
 
 let phoneToCheckStr = fs.readFileSync('phone.txt').toString();
 
-let phoneNumberArr = phoneToCheckStr.split(os.EOL).filter(el => el.trim().length > 0 && !isNaN(el));
+let phoneNumberArr = phoneToCheckStr.split(os.EOL).filter((el) => el.trim().length > 0 && !isNaN(el));
 
 let availables = [];
 
@@ -16,37 +16,37 @@ let run = async () => {
   });
   const page = await browser.newPage();
 
-  page.on('dialog', async dialog => await dialog.accept());
+  page.on('dialog', async (dialog) => await dialog.accept());
 
   await page.goto('https://web.whatsapp.com/');
 
   await page.waitForNavigation({ timeout: 0 });
 
-  await page.waitFor(3000);
+  // await page.waitFor(3000);
 
   console.log('开始检测');
 
   for (const item of phoneNumberArr) {
+    process.stdout.write(`号码: ${item}  ------- `);
     await page.goto(`https://web.whatsapp.com/send?phone=${item}&text=2333`);
-    let find = await page.waitForSelector('#app > div > span:nth-child(2) > div > span > div > div > div > div', { visible: true }).catch(() => 'timeout');
+    let find = await page.waitForSelector('#app > div > div > div:nth-child(4)', { visible: true }).catch(() => 'timeout');
 
     if (find === 'timeout') {
-      console.log(item, ' ×××××× -- 超时');
+      process.stdout.write('超时' + '\n');
       continue;
     }
 
     await page.waitFor(1000);
-    let msg = await page.$eval('#app > div > span:nth-child(2) > div > span > div > div > div > div div > div._2Vo52', el => el.textContent).catch(() => '有效');
-    if (msg === '透过网址分享的电话号码无效') {
-      console.log(item, ' ××××××');
+    if ((await page.$('#main > footer')) !== null) {
+      process.stdout.write('√√√√√√' + '\n');
     } else {
-      console.log(item, ' √√√√√√');
+      process.stdout.write('××××××' + '\n');
       availables.push(item);
     }
   }
 
   await browser.close();
-  fs.writeFile('result.csv', availables.join(os.EOL), { flag: 'w+' }, err => {});
+  fs.writeFile('result.csv', availables.join(os.EOL), { flag: 'w+' }, (err) => {});
 };
 
 run();
